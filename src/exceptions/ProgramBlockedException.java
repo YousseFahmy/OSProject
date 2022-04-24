f6fe9a5e4f17b3004b5bc69538cfc0101ea082d2
Youssef Fahmy <youssef.tfs2@gmail.com>
2022-04-24 23:28:59 +0200

    Added ProgramBlockedException

---
+++ src/exceptions/ProgramBlockedException.java
@@ -0,0 +1,13 @@
+package exceptions;
+
+public class ProgramBlockedException extends RuntimeException {
+
+	public ProgramBlockedException(){
+		super();
+	}
+	
+	public ProgramBlockedException(String message) {
+		super(message);
+	}
+	
+}
